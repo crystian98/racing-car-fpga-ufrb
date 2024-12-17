ef5f1187eb28c49c33d6f951e5fdf76af069bd45
crystian_alan <162994205+crystian98@users.noreply.github.com>
2024-12-17 14:17:27 -0300

    Update coordinate_conv.v

--- coordinate_conv.v
+++ coordinate_conv.v
@@ -1,4 +1,4 @@
-module coordenada_to_endereco(
+module coordenada_x_endereco(
     input [9:0] x,   // Coordenada horizontal (0 a 639)
     input [8:0] y,   // Coordenada vertical (0 a 479)
     output [18:0] endereco // Endereço linear (0 a 307199)
