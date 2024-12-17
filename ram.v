8eed7066c421ca4638f654c6bde98f78fa127a9a
crystian_alan <162994205+crystian98@users.noreply.github.com>
2024-12-17 14:19:12 -0300

    Update ram.v

--- ram.v
+++ ram.v
@@ -1,14 +1,14 @@
 // Módulo de memória RAM para armazenamento do frame buffer
 module memoria_ram (
     input [18:0] endereco,   // Endereço de leitura e escrita (assumindo 640x480 de pixels)
-    input [23:0] data_in,    // Dados de entrada (cor do pixel)
+    input [8:0] data_in,     // Dados de entrada (cor do pixel em 9 bits: 3 para R, G e B)
     input we,                // Sinal de escrita
     input clk,               // Clock
-    output reg [23:0] data_out  // Dados de saída (pixel lido)
+    output reg [8:0] data_out // Dados de saída (pixel lido em 9 bits)
 );
 
-    // Memória para armazenar 640x480 pixels (cada pixel é 24 bits)
-    reg [23:0] mem [0:307199]; 
+    // Memória para armazenar 640x480 pixels (cada pixel é 9 bits)
+    reg [8:0] mem [0:307199]; 
 
     always @(posedge clk) begin
         if (we) begin
